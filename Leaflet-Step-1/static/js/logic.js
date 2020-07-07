a2647ecc2f6c03ba7c96d941848ef13bf3068765
Rahul Sanghera <rahulsanghera@MacBook-Pro.local>
2020-07-06 23:49:25 -0400

    initial code

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,11 @@
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
+
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
